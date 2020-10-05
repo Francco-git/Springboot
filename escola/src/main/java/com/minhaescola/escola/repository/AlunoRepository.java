98665421587caea08593f1e3d54c38316e783278
unknown <lafeminafranco@gmail.com>
2020-10-05 17:41:24 -0300

    atualizacao

---
+++ escola/src/main/java/com/minhaescola/escola/repository/AlunoRepository.java
@@ -0,0 +1,16 @@
+package com.minhaescola.escola.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.minhaescola.escola.model.Aluno;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+	
+
+public List<Aluno> findAllBynomeContainingIgnoreCase (String nome);
+	
+	
+
+}
